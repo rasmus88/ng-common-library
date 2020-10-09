2c4d7dd370cf7423a0b8b4991960f13c14fa7807
Rasmus <rasmusadolfsson@hotmail.com>
2020-10-09 14:47:41 +0200

    Added draggable splitter component

--- src/app/components/split-test/split-test.component.ts
+++ src/app/components/split-test/split-test.component.ts
@@ -1,23 +1,86 @@
-import { Component, Input, TemplateRef } from '@angular/core';
-
+import { Component, ElementRef, HostListener, Input, Renderer2, TemplateRef, ViewChild } from '@angular/core';
+  
 @Component({
   selector: 'split-test',
   templateUrl: './split-test.component.html',
   styleUrls: ['./split-test.component.scss']
-})
+}) 
 export class SplitTestComponent {
+  @ViewChild('splitterContainer') splitterContainerRef: ElementRef;
   @Input() left: TemplateRef<any> = null;
+  @Input() handler: TemplateRef<any> = null;
   @Input() right: TemplateRef<any> = null;
   @Input() bottom: TemplateRef<any> = null;
 
-  constructor() {
+  @ViewChild('handleRef') handleElemRef: ElementRef; 
+  @ViewChild('leftRef') leftElemRef: ElementRef;
+  @ViewChild('rightRef') rightElemRef: ElementRef;
 
-  }
+  isDragging: boolean = false;
+
+  constructor(private renderer: Renderer2) { }
 
   ngAfterViewInit() {
-     console.log(`${this.left} 
-                  ${this.right}
-                  ${this.bottom}`);
+      this.renderer.listen(this.handleElemRef.nativeElement, 'mousedown', () => {
+           this.isDragging = true;
+      })
   }
 
+  @HostListener('mouseup', ['$event'])
+  nMouseUp() {  
+      this.isDragging = false;
+  }
+ 
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event) {  
+    if(this.isDragging) {
+
+      event.preventDefault();
+      // splitter container div
+      const containerNativeElem = this.splitterContainerRef.nativeElement; 
+      const boundingClientRectElem = containerNativeElem.getBoundingClientRect();
+    
+      // handle div
+      const handleNativeElem = this.handleElemRef.nativeElement;
+      const handleBoundingClientRectElem = handleNativeElem.getBoundingClientRect(); 
+      
+      let containerLeftPositionFromViewPort = boundingClientRectElem.left,
+      clientXValue = Math.round(event.clientX - containerLeftPositionFromViewPort);
+    
+      let splitterContainerWidth = boundingClientRectElem.width,
+      handleContainerWidth = handleBoundingClientRectElem.width;
+
+      let rightContainerWidth = splitterContainerWidth - handleContainerWidth - clientXValue; 
+ 
+      this.renderer.setStyle(this.splitterContainerRef.nativeElement, 'gridTemplateColumns', `${clientXValue}px ${handleContainerWidth}px ${rightContainerWidth}px`);
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  resize(event) {
+    // Left div
+    const leftNativeElem = this.leftElemRef.nativeElement; 
+    const leftBoundingClientRectElem = leftNativeElem.getBoundingClientRect();
+    
+    // Handle div
+    const handleNativeElem = this.handleElemRef.nativeElement;
+    const handleBoundingClientRectElem = handleNativeElem.getBoundingClientRect();
+
+    // Right div
+    const rightNativeElem = this.rightElemRef.nativeElement;
+    const rightBoundingClientRectElem = rightNativeElem.getBoundingClientRect();
+    
+    // total value
+    const tot = leftBoundingClientRectElem.width + handleBoundingClientRectElem.width + rightBoundingClientRectElem.width;
+
+    let leftElemPercentWidth = this.calcPercentage(leftBoundingClientRectElem.width, tot);
+    let handleElemPercentWidth = this.calcPercentage(handleBoundingClientRectElem.width, tot);
+    let rightElemPercentWidth = this.calcPercentage(rightBoundingClientRectElem.width, tot);
+ 
+    this.renderer.setStyle(this.splitterContainerRef.nativeElement, 'gridTemplateColumns', `${leftElemPercentWidth}% ${handleElemPercentWidth}% ${rightElemPercentWidth}%`); 
+  }
+  
+  private calcPercentage(partialValue: number, totalValue: number) {  
+     return (100 * partialValue) / totalValue;
+  }
 }
