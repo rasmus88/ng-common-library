976cb306827b76515afbc8f9b186c3cc3d193dd5
Rasmus <rasmusadolfsson@hotmail.com>
2020-11-02 10:07:31 +0100

    Added test

--- projects/ng-common-library/src/lib/components/read-more-modal/read-more-modal.component.spec.ts
+++ projects/ng-common-library/src/lib/components/read-more-modal/read-more-modal.component.spec.ts
@@ -1,30 +1,43 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialog } from '@angular/material/dialog';
 import { ReadMoreModalComponent } from './read-more-modal.component';
 
+@Component({
+  selector: 'test-readmoremodal',
+  template: '<read-more-modal>testusa uash uhsauh aush uahsuidh aiush iasuh uisah haisudh iaus usuususu uasuususu uaus uasusuu ausu uaisuusu hasiuh sh aisusaisai uods sdoodso ksdodsok oskdodsk osdksdo ksodok kosdk dsok dso kdsokdosko osdk kosdkoko ksodko skokoso kokskdo kodskokoko kosko ksodk oaisiusa uasiu aisua i iuu aii uuuu</read-more-modal>'
+})
+class TestReadMoreModalComponent { }
+
 describe('ReadMoreModalComponent', () => {
   let component: ReadMoreModalComponent;
-  let fixture: ComponentFixture<ReadMoreModalComponent>;
+  let fixture: ComponentFixture<TestReadMoreModalComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReadMoreModalComponent ],
-      providers: [
-        { 
-          provide: MatDialog, useValue: {}
-        }
-      ],
+      declarations: [ ReadMoreModalComponent, TestReadMoreModalComponent ],
+      providers: [{ provide: MatDialog, useValue: {} }],
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ReadMoreModalComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestReadMoreModalComponent);
+    component = fixture.debugElement.childNodes[0].componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have text content', () => {
+    const content = component.contentTextRef.nativeElement.innerHTML;
+    expect(content.length >= 1).toBeTrue();
+  })
+
+  it('should have show more text visible', () => {
+    let showMoreText = component.divElementRef.nativeElement.parentElement.querySelector('.div-collapse').innerText;
+    expect(showMoreText).toEqual('Show more')
   });
 });
